fb7868704223cdc8a03f9aac822f704476bb8c3f
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2021-03-15 14:12:28 -0300

    subindo pro git

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fee36a93361827581e958c5084f0402a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '84e429d8-ab5a-4004-be1e-a559a2931911'
+};
